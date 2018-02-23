a0ed90c908ee7fb7bdc3837c38d6ff1229e40362
Eddie Yao <adpoyao@gmail.com>
2018-02-23 12:59:05 -0800

    Touched up CSS.

--- nothedroids_server/config.js
+++ nothedroids_server/config.js
@@ -5,7 +5,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://dev:dev@ds147228.mlab.com:47228/nothedroids',
   JWT_SECRET: process.env.JWT_SECRET || 'hireeddie',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
